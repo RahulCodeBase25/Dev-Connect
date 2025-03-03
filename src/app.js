fc0d05b6e9d510dec043b48e258a24cb962782e4
RahulCodeBase25 <rahul.codecraft25@gmail.com>
2025-03-03 22:40:50 +0530

    query,params and reading the data

--- src/app.js
+++ src/app.js
@@ -13,7 +13,13 @@ app.use("/about" , (req,res)=>{
     res.send("This is my about page..");
 })
 
-app.get("/user" , (req,res)=>{
+app.get("/user/:userId/:ghar/:gym" , (req,res)=>{
+    //this is how you read your data
+    console.log(req.query); //just to do this "/user?userid=101"
+    console.log(req.params); //this is how you read the parameters of routes given after ":"
+    //":" -colon means dynamic routes
+    
+    
     res.send({firstName: "Rahul", lastName:"Singh"})
 })
 
@@ -23,6 +29,14 @@ app.post("/user" , (req,res)=>{
 
 app.delete("/user" , (req,res)=>{
     res.send("Deleted Successfully!!!")
+})
+
+//Here "?" represents that ,whatever is before ? mark is optional ,ex:->rahul and rahl both will work
+//and "+" means before rahuuuuuuul can write like this but rahullll is now allowed
+//and "*" means(rah*ul), if i write rahhckhdhcdhcdhul it will work, but i have to make sure that my word should only start with "rah" and ends with "ul"
+app.use("/ra(hu)?l" , (req,res)=>{  //if i do like this,that means "hu" is optional
+    //and this() things works with * , + , ? and all.
+    res.send("learning about ? and +..advance routing technique")
 })
 
 app.listen(7777, ()=>{
