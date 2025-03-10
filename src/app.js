1c6b4b4f7c313025e1fe0156406a63a949ca50b1
RahulCodeBase25 <rahul.codecraft25@gmail.com>
2025-03-10 18:11:58 +0530

    read the cookies in profile,created user auth MW, added that in profile, and added expiry in token and cookies

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const User = require("./models/user");
 const { validateSignUpdata } = require("./utils/validation");
 const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const {userAuth} = require ("./middlewares/auth")
 
 app.use(express.json());
 app.use(cookieParser());
@@ -36,120 +37,35 @@ app.post("/login" , async (req,res)=>{
     if(!user){
       throw new Error("Invalid Credentials❌...")
     }
-
     const isPasswordvalid = await bcrypt.compare(password , user.password);
     if(isPasswordvalid){
-
       //after eating cookie, lets add jwt:->
-      const token =await jwt.sign({_id : user._id},"Singh5771@");
-      console.log(token);
-      
-
+      const token =await jwt.sign({_id : user._id},"Singh5771@" , {expiresIn : "7d"}); //7d means 7days
       //Now, i am adding the token to cookie and sending the response back to user
-      res.cookie("token" , token)
-
+      res.cookie("token" , token , {expires: new Date(Date.now()+ 8 * 3600000)}) //upar bnaya token expire hone ka ye hai 8 hours mai cookies expire hone ka , we can see the date in cookies postman
       res.send("Login Successfull🥳...")
     }
     else{
-      throw new Error("Invalid Credentials❌...")
+      throw new Error("Invalid Credentials🙅‍♂️...")
     }
-
-
   }catch(err){
     res.status(400).send("ERROR: " + err.message);
   }
 })
 
-app.get("/profile" , async (req , res)=>{
+app.get("/profile" ,userAuth, async (req , res)=>{
   try{
-  const cookies = req.cookies;
-  const {token} = cookies;  //Ek route se dusre route mai agar kuch liya to {} lgane pdte hai...
-  if(!token){
-    throw new Error("Invalid token!!...")
-  }
-  const decodedMsg = await jwt.verify(token, "Singh5771@");
-  const {_id} = decodedMsg; 
-  const user = await User.findById(_id);
-  if(!user){
-    throw new Error("User not found...");
-  }
+    const user = req.user;
    res.send(user);
 }catch(err){
   res.status(400).send("ERROR : " + err.message);
   }
 })
-
-
-app.get("/user" ,async (req,res)=>{
-  const userEmail = req.body.emailId;
-  try{
-    const user = await User.findOne({emailId : userEmail});
-    if(!user){
-      res.status(404).send("User does not exist!!")
-    }else{
-      res.send(user)
-    }
-    
-  }
-
-  // try{
-  //   const users = await User.find({emailId : userEmail});
-  //   if(users.length ===0){
-  //     res.status(404).send("User Email not found..")
-  //   }else{
-  //     res.send(users);
-  //   }
-  // }
-  catch(err){
-    res.status(400).send("Email id not found" + err.message);
-  }
+app.post("/sendConnectionReq" ,userAuth, (req,res)=>{
+  const user = req.user; //This is how i can read who is the user here..
+  console.log('Sending a connection request but auth became easy now..');
+  res.send(user.firstName + "Sended you a request🤭")  
 })
-
-app.delete("/user" , async (req,res)=>{
-  const userId = req.body.userId;
-  try{
-    // const user = await User.findByIdAndDelete({_id : userId}); //i can also pass like this✅
-    const user = await User.findByIdAndDelete(userId);
-    res.send("User deleted Successfully!!")
-  }catch(err){
-    res.status(404).send("The user you're trying to delete is not found..")
-  }
-})
-
-//Lets create a update Api now:->
-app.patch("/user/:userId", async(req, res)=>{
-  const userId = req.params?.userId ; //Now userId is imp whenever i need to upate any users data.
-  const data = req.body;  
-  try{
-    const ALLOWED_UPDATES=["photoUrl","about","skills","gender", "age"];
-
-  const isUpdateAllowed = Object.keys(data).every((k)=>ALLOWED_UPDATES.includes(k)); //I should remember this
-  if(!isUpdateAllowed){
-    throw new Error("Updates not allowed...");
-  }
-  if(data?.skills.length > 5){
-    throw new Error("You can add only 5 skills here...")
-  }
-    const user = await User.findByIdAndUpdate({_id : userId} ,
-    data , 
-    {returnDocument : "before",runValidators:true} //Now bcz i added validations in my database so i have to add this "runValidators" field in my obj
-    );
-    //This returnDocument basically show karta hai apna latest data "before" and "after" ke baad..
-    res.send("User has been updated");
-  }catch(err){
-    res.status(404).send("User updation failed!!" + err.message)
-  }
-})
-
-app.get("/feed" ,async (req,res)=>{
-  try{
-    const users = await User.find({});
-    res.send(users);
-  }catch(err){
-    res.status(400).send("Something Went Wrong!!!")
-  }
-})
-
 dbConnect()
 .then(()=>{
     console.log('Database Connection Established✅..');
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,28 +1,28 @@
-const adminAuth = (req,res,next)=>{
-    console.log('Admin auth is getting checked');
-    
-  const token = "aaxyz";
-  const isAdminAuthorized = token==="xyz";
-  if(!isAdminAuthorized){
-    res.status(401).send("Not authorized")
-  }else{
-    next();
-  }
-  }
+const jwt = require("jsonwebtoken");
+const User = require("../models/user")
 
-  const userAuth = (req,res,next)=>{
-    console.log('Admin auth is getting checked');
-    
-  const token = "xyz";
-  const isAdminAuthorized = token==="xyz";
-  if(!isAdminAuthorized){
-    res.status(401).send("Not authorized")
-  }else{
-    next();
+const userAuth =async (req,res,next)=>{
+  //1)->Read the token from the req cookies
+  try{
+  const {token} = req.cookies;  
+  if(!token){
+    throw new Error("Invalid token!!🎟️🎟️...")
   }
+  const decodedObj = await jwt.verify(token , "Singh5771@");
+
+  //2)->Validate the token
+  const{_id} = decodedObj;
+  const user = await User.findById(_id)
+  if(!user){
+    throw new Error("User not found👤...")
+  }
+  req.user= user; //Now from Auth we will get this user ans send it back to the further process is /profile
+  next(); //bcz its the middleware so we can easily use the next function...
+}catch(err){
+  res.status(400).send("ERROR : "+err.message)
+}
   }
 
   module.exports = {  //normal object ke form mai exports kiya...
-    adminAuth,
     userAuth ,
   }
